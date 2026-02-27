0fe0d24a9e945c4252e376421b46eb5966bf1bd9
Nitesh14022004 <niteshsince04@gmail.com>
2026-02-27 17:38:29 +0530

    Change minifier from terser to esbuild in vite config

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-    minify: 'terser',
+    minify: 'esbuild',
     rollupOptions: {
       output: {
         manualChunks: {
